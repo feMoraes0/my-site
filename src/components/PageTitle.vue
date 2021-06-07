3dc764dec72277d0863bbc1fa7680e0950954c6c
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-06-07 20:24:02 -0300

    Add page title component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1><slot></slot></h1>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+};
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 70px;
+    font-weight: 700;
+  }
+</style>
