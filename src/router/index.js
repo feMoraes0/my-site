de6f65b56f7a789831f87c220b8f2a8aca92c428
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-06-09 20:55:23 -0300

    Remove image and add new route

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue'),
   },
+  {
+    path: '/projects',
+    name: 'Projects',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Projects.vue'),
+  },
 ];
 
 const router = new VueRouter({
